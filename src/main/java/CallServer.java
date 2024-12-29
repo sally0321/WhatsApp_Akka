a23d7cc7bed66ed06db636d7d110364049b8889f
ChanYaWen <167830910+ChanYaWen@users.noreply.github.com>
2024-12-29 22:15:13 +0800

    Update CallServer.java

--- src/main/java/CallServer.java
+++ src/main/java/CallServer.java
@@ -8,9 +8,9 @@ import java.util.Map;
 
 public class CallServer extends AbstractActor {
 
-    //private final Map<String, ActorRef> clients = new HashMap<>();
+    private final Map<String, ActorRef> userActors = new HashMap<>();
     private final Map<ActorRef, ActorRef> activeCalls = new HashMap<>();
-    private Map<String, ActorRef> userActors = new HashMap<>();
+
     public static Props props() {
         return Props.create(CallServer.class, CallServer::new);
     }
@@ -18,54 +18,80 @@ public class CallServer extends AbstractActor {
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(CallServer.DisconnectUser.class, message -> {
-                    System.out.println("Server disconnect " + message.username);
-                    System.out.println(userActors);
-                    userActors.remove(message.username);
-                    //getSender().tell(message.username + " log in successfully!", getSelf());
-                })
-                .match(CallServer.ConnectUser.class, message -> {
-                    System.out.println("Server connect " + message.username);
-                    System.out.println(userActors);
+                // Handle user registration
+                .match(ConnectUser.class, message -> {
+                    System.out.println("Server connected: " + message.username);
                     userActors.put(message.username, message.userActor);
-                    //getSender().tell(message.username + " log in successfully!", getSelf());
                 })
-
-                /*
-                .match(RegisterClient.class, msg -> {
-                    userActors.put(msg.username, getSender());
-                    System.out.println("User registered: " + msg.username);
-                })*/
+                // Handle user disconnection
+                .match(DisconnectUser.class, message -> {
+                    System.out.println("Server disconnected: " + message.username);
+                    userActors.remove(message.username);
+                })
+                // Handle initiating a call
                 .match(InitiateCall.class, msg -> {
                     ActorRef target = userActors.get(msg.targetUsername);
                     if (target != null) {
                         target.tell(new IncomingCall(msg.callerUsername), getSelf());
                         activeCalls.put(getSender(), target);
+                        System.out.println(msg.callerUsername + " initiated a call to " + msg.targetUsername);
                     } else {
                         getSender().tell(new CallFailed(msg.targetUsername + " is not online."), getSelf());
                     }
                 })
+                // Handle string-based user inputs
+                .match(String.class, input -> {
+                    ActorRef caller = activeCalls.get(getSender());
+                    if ("Y".equalsIgnoreCase(input)) {
+                        // Accept the call
+                        if (caller != null) {
+                            caller.tell(new CallAccepted(getSender().path().name()), getSelf());
+                            System.out.println("Call accepted by " + getSender().path().name());
+                        }
+                    } else if ("N".equalsIgnoreCase(input)) {
+                        // Reject the call
+                        if (caller != null) {
+                            caller.tell(new CallRejected(getSender().path().name()), getSelf());
+                            activeCalls.remove(getSender());
+                            System.out.println("Call rejected by " + getSender().path().name());
+                        }
+                    } else if ("0".equals(input)) {
+                        // End the call
+                        if (caller != null) {
+                            caller.tell(new CallEnded(getSender().path().name()), getSelf());
+                            activeCalls.remove(getSender());
+                            System.out.println("Call ended by " + getSender().path().name());
+                        } else {
+                            System.out.println("No active call found for " + getSender().path().name());
+                        }
+                    } else {
+                        System.out.println("Invalid input: " + input);
+                    }
+                })
+                // Handle call response (accept/reject)
                 .match(RespondCall.class, msg -> {
                     ActorRef caller = activeCalls.get(getSender());
                     if (caller != null) {
                         if (msg.accepted) {
-                            caller.tell(new CallAccepted(msg.responderUsername), getSelf());
-                            getSender().tell(new CallAccepted(msg.responderUsername), getSelf());
+                            caller.tell(new CallAccepted(msg.targetUsername), getSelf());
                         } else {
-                            caller.tell(new CallRejected(msg.responderUsername), getSelf());
+                            caller.tell(new CallRejected(msg.targetUsername), getSelf());
                             activeCalls.remove(getSender());
                         }
                     }
                 })
+                // Handle ending a call
                 .match(EndCall.class, msg -> {
                     ActorRef other = activeCalls.remove(getSender());
                     if (other != null) {
-                        other.tell(new CallServer.CallEnded(msg.username), getSelf());
+                        other.tell(new CallEnded(msg.username), getSelf());
                         activeCalls.remove(other);
+                        System.out.println("Call ended by " + msg.username);
                     } else {
-                        System.out.println("The call has ended.");
+                        System.out.println("No active call to end for " + msg.username);
                     }
                 })
+
                 .build();
     }
 
@@ -80,20 +106,11 @@ public class CallServer extends AbstractActor {
         }
     }
 
-    public static class DisconnectUser extends ChatServer.ConnectUser {
+    public static class DisconnectUser extends ConnectUser {
         public DisconnectUser(String username, ActorRef userActor) {
             super(username, userActor);
         }
     }
-
-    /*
-    public static class RegisterClient implements Serializable {
-        public final String username;
-
-        public RegisterClient(String username) {
-            this.username = username;
-        }
-    }*/
 
     public static class InitiateCall implements Serializable {
         public final String callerUsername;
@@ -106,11 +123,11 @@ public class CallServer extends AbstractActor {
     }
 
     public static class RespondCall implements Serializable {
-        public final String responderUsername;
+        public final String targetUsername;
         public final boolean accepted;
 
         public RespondCall(String responderUsername, boolean accepted) {
-            this.responderUsername = responderUsername;
+            this.targetUsername = responderUsername;
             this.accepted = accepted;
         }
     }
